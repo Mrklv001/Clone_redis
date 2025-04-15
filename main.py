5037e4a6a26152fcb249b73d2c05922490d10c6e
Markelov A <mrklv001@gmail.com>
2025-04-15 20:56:55 +0300

    Added a project

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+import socket
+
+
+async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    while True:
+        data = await reader.read(8192)
+        if not data:
+            break
+
+        writer.write("+PONG\r\n".encode())
+        await writer.drain()
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main() -> None:
+    server = await asyncio.start_server(handle_client, "localhost", 6379, reuse_port=True)
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
